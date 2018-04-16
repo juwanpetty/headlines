99744afb47c7a4b9bcb6e3ba87befe325f6ba9a3
juwanpetty <jchpetty@gmail.com>
2018-04-16 00:36:53 -0400

    Add infinite slider

---
+++ js/main.js
@@ -0,0 +1,22 @@
+feather.replace();
+
+$('.source-logos').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+        pauseOnHover: false,
+        responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 3
+        }
+    }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 2
+        }
+    }]
+});
