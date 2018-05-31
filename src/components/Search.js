d5704eeff31022e79b84d0c5e94133b58877f3b0
juwanpetty <jchpetty@gmail.com>
2018-05-31 10:08:26 -0400

    Fix for input value attribute

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ export default class Search extends React.Component {
             <div className="search">
                 <form method="get" action="http://www.google.com/search?q=">
                     <div className="search__wrapper">
-                        <input type="text" className="searchbar" value="" placeholder="Search the Web" name="q" autoComplete="off" />
+                        <input type="text" className="searchbar" placeholder="Search the Web" name="q" autoComplete="off" />
                         <div className="search__icon"><i data-feather="search"></i></div>
                         <button type="submit"><i data-feather="arrow-right"></i></button>
                     </div>
