ca28ea7a39b3f80bfc245329c14b7ac3fe4e762a
juwanpetty <jchpetty@gmail.com>
2018-05-31 10:07:52 -0400

    Add method to replace image with placeholder if problems exist

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 export default class Article extends React.Component {
+    constructor(props) {
+        super(props);
+        this.checkImage = this.checkImage.bind(this);
+    }
+
+    checkImage(e) {
+        // placeholder if no image path exists
+        if (e.target.src === null) {
+            return e.target.src = "assets/placeholder.jpg";
+        }
+
+        // displays placeholder if img is 1px (IGN)
+        if (e.target.naturalWidth <= 1) {
+            return e.target.src = "assets/placeholder.jpg";
+        }
+    }
+
     render() {
-        const { source, title, description, url, urlToImage } = this.props.article
+        const { source, title, description, url, urlToImage } = this.props.article;
 
         return (
             <li className="article__item">
                 <a href={url}>
                     <div className="article__image__container">
-                        <img className="article__image" src={urlToImage} alt={title} />
+                        <img className="article__image" onLoad={this.checkImage} src={urlToImage} alt={title} />
                     </div>
                     <div className="article__details">
                         <p className="article__source">{source.name}</p>
