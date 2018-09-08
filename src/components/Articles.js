e25c5f2957f3c71a127e72dbf3863a590dbbf57a
juwanpetty <jchpetty@gmail.com>
2018-09-08 14:22:24 -0400

    Tweaked articles keleton design, more comparable to loaded content

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -9,7 +9,7 @@ export default class Articles extends React.Component {
 
         if (articleError) {
             return <div >Error: {error.message}</div>;
-        } else if (!articleIsLoaded) {
+        } else if (true) {
             return (
                 <div className={this.props.showArticles ? "articles" : "articles articles--hidden"}>
                     <ArticleSkeleton />
