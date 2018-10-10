c5ef4f24ee63b9c5777ec4cfafe12b2b424e7119
juwanpetty <jchpetty@gmail.com>
2018-10-10 08:32:28 -0400

    Add quick tags for sources

---
+++ src/js/components/Sidebar/components/SourcesTags/SourcesTags.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import FeatherIcon from 'feather-icons-react';
+import styles from './SourcesTags.scss';
+
+export default class SourcesTags extends React.Component {
+    removeTag(event) {
+        const sourceId = event.target.data.id;
+
+        console.log(sourceId);
+        // this.props.handleDeleteSource(sourceId);
+    }
+
+    render() {
+        const { storedSources, sources } = this.props;
+        
+        if (storedSources) {
+            return (
+                <div className={styles.SourceTags}>
+                    {
+                        storedSources.map((storedSources, index) => {
+                            return (
+                                sources.map(source => {
+                                    if (storedSources === source.id) {
+                                        return (
+                                            <div className={styles.Tag} key={index}>
+                                                <div className={styles.Container}>
+                                                    <p>{source.name}</p>
+                                                    <div 
+                                                        className={styles.Icon}
+                                                        data-id={source.id}
+                                                        onClick={this.removeTag}
+                                                    >
+                                                        <FeatherIcon icon="x" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                })
+                            )
+                        })
+                    }
+                </div>
+            );
+        }  else {
+            return null;
+        }
+    }
+}
