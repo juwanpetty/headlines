e4f2824723d105a70371ada0e85902699b1a13a3
juwanpetty <jchpetty@gmail.com>
2018-11-20 00:16:54 -0500

    Initial files

---
+++ src/js/components/TopSites/TopSites.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+import styles from './TopSites.scss';
+
+export default class TopSites extends Component {
+  render() {
+    return(
+      <div>
+        <div>Words</div>
+      </div>
+    );
+  }
+}
