daefd821ee7d88852d1612a209db5264c552ce40
juwanpetty <jchpetty@gmail.com>
2019-01-13 17:45:27 -0500

    Add edge case for wrong search params

--- src/js/components/Search/Search.js
+++ src/js/components/Search/Search.js
@@ -8,15 +8,33 @@ export default class Search extends React.Component {
         this.handleSearchEngine = this.handleSearchEngine.bind(this)
 
         this.state = {
-            currentSearchEngine: 'Google'
+            currentSearchEngine: 'Google',
+            search: {
+                "Google": {
+                    "searchParams": "http://www.google.com/search?q=",
+                    "src": "assets/search/google.png"
+                },
+                "DuckDuckGo": {
+                    "searchParams": "https://duckduckgo.com/?q=",
+                    "src": "assets/search/duckduckgo.png"
+                },
+                "Bing": {
+                    "searchParams": "http://www.bing.com/search?q=",
+                    "src": "assets/search/bing.png"
+                }
+            }
         }
     }
 
     componentDidMount() {
         if (localStorage.currentSearchEngine) {
-            this.setState({ 
-                currentSearchEngine: localStorage.getItem('currentSearchEngine')
-            });
+            if (Object.keys(this.state.search).includes(localStorage.currentSearchEngine)) {
+                this.setState({ 
+                    currentSearchEngine: localStorage.getItem('currentSearchEngine')
+                });
+            } else {
+                localStorage.setItem('currentSearchEngine', 'Google');
+            }
         } else {
             localStorage.setItem('currentSearchEngine', 'Google');
         }
@@ -37,29 +55,17 @@ export default class Search extends React.Component {
     }
 
     render() {
-        const search = {
-            "Google": {
-                "searchParams": "http://www.google.com/search?q=",
-                "src": "assets/search/google.png"
-            },
-            "DuckDuckGo": {
-                "searchParams": "https://duckduckgo.com/?q=",
-                "src": "assets/search/duckduckgo.png"
-            },
-            "Bing": {
-                "searchParams": "http://www.bing.com/search?q=",
-                "src": "assets/search/bing.png"
-            }
-        };
+        const searchParams = this.state.search[this.state.currentSearchEngine].searchParams;
+        const imageSrc = this.state.search[this.state.currentSearchEngine].src;
 
         return (
             <div className={styles.Search}>
-                <form method="get" action={search[this.state.currentSearchEngine].searchParams}>
+                <form method="get" action={searchParams}>
                     <div className={styles.Wrapper}>
                         <input type="text" className={styles.SearchBar} placeholder="Search the Web" name="q" autoComplete="off" id="search" />
                         <div className={styles.SearchEngine} htmlFor="search" onClick={this.props.toggleSearchMenu}>
                             <div>
-                                <img src={search[this.state.currentSearchEngine].src}/>
+                                <img src={imageSrc}/>
                             </div>
                         </div>
                         <button type="submit"><i data-feather="arrow-right"></i></button>
@@ -70,19 +76,19 @@ export default class Search extends React.Component {
                     <ul className={styles.Dropdown} onClick={this.handleSearchEngine}>
                         <li>
                             <div className={styles.Container}>
-                                <img src={search["Google"].src} alt=""/>
+                                <img src={this.state.search["Google"].src} alt=""/>
                             </div>
                             <p>Google</p>
                         </li>
                         <li>
                             <div className={styles.Container}>
-                                <img src={search["DuckDuckGo"].src} alt=""/>
+                                <img src={this.state.search["DuckDuckGo"].src} alt=""/>
                             </div>
                             <p>DuckDuckGo</p>
                         </li>
                         <li>
                             <div className={styles.Container}>
-                                <img src={search["Bing"].src} alt=""/>
+                                <img src={this.state.search["Bing"].src} alt=""/>
                             </div>
                             <p>Bing</p>
                         </li>
