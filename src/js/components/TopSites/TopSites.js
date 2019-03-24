54edf1d915eccaf7a03311792d4f0e563cbd109b
juwanpetty <jchpetty@gmail.com>
2019-03-24 12:33:01 -0400

    Fix broken images

--- src/js/components/TopSites/TopSites.js
+++ src/js/components/TopSites/TopSites.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react';
-
-const ellipsize = require('ellipsize');
-
+import TopSite from './components/TopSite';
 import styles from './TopSites.scss';
 
 export default function TopSites() {
   const [sites, setSites] = useState([]);
+  const [favicon, setFavicon] = useState();
 
   useEffect(() => {
     try {
@@ -15,25 +14,11 @@ export default function TopSites() {
     } catch (error) {}
   }, []);
 
-  const handleDomainFavicon = (url) => {
-    return url.endsWith('/') ? `${url}favicon.ico` : `${url}/favicon.ico`;
-  };
-
   return (
     <ul className={styles.Sites}>
       {sites &&
         sites.map((site) => {
-          return (
-            <li key={site.title}>
-              <a href={site.url} className={styles.Site}>
-                <div className={styles.Favicon}>
-                  <img src={handleDomainFavicon(site.url)} alt={site.url} />
-                </div>
-
-                <p className={styles.Title}>{ellipsize(site.title, 25)}</p>
-              </a>
-            </li>
-          );
+          return <TopSite key={site.title} site={site} />;
         })}
     </ul>
   );
