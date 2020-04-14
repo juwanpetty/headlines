742d4fc1b1915c1d1410f6794120edfce1570895
juwanpetty <jchpetty@gmail.com>
2020-04-13 22:59:14 -0400

    Add redux to project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 import { App } from "./App";
 
+const store = configureStore();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
