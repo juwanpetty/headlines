64517ef663ac07581afdc2a6b9b18a34743654d4
juwanpetty <jchpetty@gmail.com>
2020-04-21 22:51:30 -0400

    Update default ui states

--- src/store/slices/ui.js
+++ src/store/slices/ui.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const initialState = {
   darkTheme: false,
-  isSidebarOpen: true,
+  isSidebarOpen: false,
 };
 
 const uiSlice = createSlice({
