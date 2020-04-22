3076f9ba1293fc1fa153ed7a695f4c25bb752c90
juwanpetty <jchpetty@gmail.com>
2020-04-21 22:43:43 -0400

    Update clock formats

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { clockSelector } from "../../store/slices/clock";
 import { Container } from "./Clock.module";
 
 export const Clock = () => {
+  const { timeFormat } = useSelector(clockSelector);
+  const isTimeFormat12Hour = timeFormat === "12" ? true : false;
   const [date, setDate] = useState(
-    new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+    new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: isTimeFormat12Hour,
+    })
   );
 
   useEffect(() => {
@@ -16,7 +24,11 @@ export const Clock = () => {
 
   const tick = () => {
     setDate(
-      new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+      new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: isTimeFormat12Hour,
+      })
     );
   };
 
