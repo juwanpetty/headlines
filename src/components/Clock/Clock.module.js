b89e53fefa723f2b77171d19c041f68216f484a1
juwanpetty <jchpetty@gmail.com>
2020-04-29 11:44:03 -0400

    Update styles

--- src/components/Clock/Clock.module.js
+++ src/components/Clock/Clock.module.js
@@ -2,20 +2,20 @@ import styled from "styled-components";
 import { typography } from "../../constants/styles";
 
 export const Container = styled.div`
-  margin: 0;
   display: flex;
   align-items: center;
+  margin-bottom: 5rem;
 
   span {
     font-size: ${typography.size.m2}rem;
-    margin: 0 24px;
+    margin: 0 16px;
   }
 `;
 
 export const Content = styled.h2`
   font-variant-numeric: tabular-nums;
-  font-size: ${typography.size.l2}rem;
-  margin-bottom: 5rem;
+  font-size: ${typography.size.l1}rem;
+  margin: 0;
 `;
 
 export const TimeText = styled(Content)`
